1c9780d6becc4ec439a2549ee0dbe4f93331006e
Jesus <jjcorrales76@gmail.com>
2025-07-21 12:46:23 -0600

    image grid with tailwind

--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -1,29 +1,28 @@
 import fetchImages from "@/lib/fetchImages";
-import type { ImagesResults } from "@/models/images"
-import ImgContainer from "./ImgContainer"
-import addBlurredDataUrls from "@/lib/getBase64"
+import type { ImagesResults } from "@/models/images";
+import ImgContainer from "./ImgContainer";
+import addBlurredDataUrls from "@/lib/getBase64";
 
 type Props = {
   topic?: string | undefined;
 };
 
-export default async function Gallery({topic}: Props) {
-  const url = !topic 
-  ? "https://api.pexels.com/v1/curated"
-  : `https://api.pexels.com/v1/search?query=${topic}`;
-
+export default async function Gallery({ topic }: Props) {
+  const url = !topic
+    ? "https://api.pexels.com/v1/curated"
+    : `https://api.pexels.com/v1/search?query=${topic}`;
 
   const images: ImagesResults | undefined = await fetchImages(url);
 
   if (!images)
     return <h2 className="m-4 text-2xl font-bold">No Images Found</h2>;
 
-  const photosWithBlur = await addBlurredDataUrls(images)
+  const photosWithBlur = await addBlurredDataUrls(images);
 
   return (
-    <section className="px-2 my-3 grid gap-2 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-      {photosWithBlur.map(photo => (
-                <ImgContainer key={photo.id} photo={photo} />
+    <section className="px-1 my-3 grid gap-2 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      {photosWithBlur.map((photo) => (
+        <ImgContainer key={photo.id} photo={photo} />
       ))}
     </section>
   );
--- src/app/components/ImgContainer.tsx
+++ src/app/components/ImgContainer.tsx
@@ -1,21 +1,35 @@
-import type { Photo } from "@/models/images";
-import Image from "next/image";
+import type { Photo } from "@/models/images"
+import Image from "next/image"
+import Link from "next/link"
 
 type Props = {
-  photo: Photo;
-};
+    photo: Photo
+}
 
 export default function ImgContainer({ photo }: Props) {
-  return (
-    <div className="h-64 bg-gray-200 rounded-xl relative overflow-hidden shadow-md group">
-      <Image
-        src={photo.src.large}
-        alt={photo.alt}
-        fill={true}
-        placeholder="blur"
-        blurDataURL={photo.blurredDataUrl}
-        className="object-cover rounded-xl group-hover:opacity-75"
-      />
-    </div>
-  );
+
+    const widthHeightRatio = photo.height / photo.width
+    const galleryHeight = Math.ceil(250 * widthHeightRatio)
+    const photoSpans = Math.ceil(galleryHeight / 10) + 1
+
+    return (
+        <div className="w-[250px] justify-self-center"
+            style={{ gridRow: `span ${photoSpans}` }}
+        >
+            <Link href={photo.url} target="_blank" className="grid place-content-center">
+                <div className="rounded-xl overflow-hidden group">
+                    <Image
+                        src={photo.src.large}
+                        alt={photo.alt}
+                        width={photo.width}
+                        height={photo.height}
+                        sizes="250px"
+                        placeholder="blur"
+                        blurDataURL={photo.blurredDataUrl}
+                        className="group-hover:opacity-75"
+                    />
+                </div>
+            </Link>
+        </div>
+    )
 }
